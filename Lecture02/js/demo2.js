e288fae8b02309d53f9904ad9e5b7aad35288a9c
hooglander <42916767+hooglander@users.noreply.github.com>
2020-04-05 20:00:08 +0200

    final

--- Lecture02/js/demo2.js
+++ Lecture02/js/demo2.js
@@ -1,22 +1,23 @@
+var height = parseFloat(prompt("Insert your height (cm):"));
 var weight = parseInt(prompt("Insert your weight (kg):"));
-var height = parseInt(prompt("Insert your height (cm):"));
+// Compare parseInt with parseFloat for float inputs
 
-if (typeof weight != Number || typeof height != Number) {
+if (typeof height != Number || typeof number != Number) {
   console.log("Invalid input!");
 }
 
-height = height/100;
-// const formula = weight/(height*height)
-const formula = weight/(Math.pow(height, 2));
-let result = 'Your BMI is ' + formula.toFixed(2);
-// console.log(result);
+height = height/100;  // To convert cm to m
+const formula = weight/(height*height);
+
+let result = `Your BMI is ${formula}`;
+// alternatively: let result = 'Your BMI is ' + formula;
 
 if (formula < 18.5) {
-  console.log(result + "\nYou are underweight")
+  console.log(result + "\nYou are underweight");
 } else if (formula >= 18.5 && formula < 24.9) {
-  console.log(result + "\nYou have a normal weight")
+  console.log(result + "\nYou have a normal weight");
 } else if (formula >= 25 && formula < 29.9) {
-  console.log(result + "\nYou are overwright")
+  console.log(result + "\nYou are overwright");
 } else if (formula >= 30) {
-  console.log(result + "\nYou are obese")
+  console.log(result + "\nYou are obese");
 }
